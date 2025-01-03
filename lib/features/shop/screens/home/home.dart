2bb3a42c2efdf57ef554b0f2ac1513331d17271d
Acer <vtharu2004@gmail.com>
2025-01-03 14:50:38 +0700

    Custom AppBar

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -1,10 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_elearning_project/common/products/cart/cart_menu_icon.dart';
-import 'package:flutter_elearning_project/common/widgets/appbar/appbar.dart';
 import 'package:flutter_elearning_project/common/widgets/custom_shapes/container/primary_header_container.dart';
 import 'package:flutter_elearning_project/features/shop/screens/home/widgets/home_appbar.dart';
-import 'package:flutter_elearning_project/utils/constants/colors.dart';
-import 'package:flutter_elearning_project/utils/constants/text_strings.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
