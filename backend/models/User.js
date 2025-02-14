005504d9f7f5260b581899b2db8e1f3ee476c1a6
Acer <vtharu2004@gmail.com>
2025-02-15 00:14:37 +0700

    Fix index full

--- backend/models/User.js
+++ backend/models/User.js
@@ -22,12 +22,10 @@ const User = sequelize.define('User', {
   },
   username: {
     type: DataTypes.STRING,
-    unique: true,
     allowNull: false
   },
   email: {
     type: DataTypes.STRING,
-    unique: true,
     allowNull: false,
     validate: {
       isEmail: true
